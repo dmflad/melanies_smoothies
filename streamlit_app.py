ff24b086dfc2c7d6e76b042059bea2ac7810e9c0
dmflad <44819407+dmflad@users.noreply.github.com>
2025-05-23 10:16:44 -0400

    Update streamlit_app.py
    Added new section to display smoothiefroot nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,10 @@ if ingredients_list:
         #st.stop()
         
         st.success('Your Smoothie is ordered !', icon="✅")
+
+
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
