0bc75ea6bc311ef53d9bf99d9e934f6caebb1e15
dmflad <44819407+dmflad@users.noreply.github.com>
2025-05-23 11:47:38 -0400

    Update streamlit_app.py
    get one row per fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,7 @@ import pandas as pd
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :banana::pear::mango:")
-st.write(
-  """Choose the fruits you want in your custom Smoothie!"""
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie: ')
 st.write("The name on your smoothie will be: ", name_on_order)
@@ -21,8 +19,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to five (5) ingredients:'
@@ -33,6 +31,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
